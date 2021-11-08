87f26b0e42b5097e210405f556b44b1833aa6a24
Noobuster <nhanluu838@gmail.com>
2021-11-08 17:34:42 +0700

    fb for be

--- api/authenticate/index.js
+++ api/authenticate/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require("../../modules/passport");
 const jwt = require("jsonwebtoken");
 const pool = require("../../config-db");
-const { OAuth2Client } = require('google-auth-library');
+const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(process.env.CLIENT_ID_GOOGLE);
 
 router.post(
@@ -19,32 +19,45 @@ router.post(
   }
 );
 
-router.post(
-  "/google",
-  async function (req, res) {
-    try {
-      const ticket = await client.verifyIdToken({
-        idToken: req.body.token,
-        audience: process.env.CLIENT_ID_GOOGLE
-      });
-      const payload = ticket.getPayload();
-      const user = {
-        id: payload['sub'],
-        first_name: payload['given_name'],
-        last_name: payload['family_name'],
-        name: payload['email'],
-        avatar: payload['picture']
-      };
-      res.json({
+router.post("/google", async function (req, res) {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: req.body.token,
+      audience: process.env.CLIENT_ID_GOOGLE,
+    });
+    const payload = ticket.getPayload();
+    const user = {
+      id: payload["sub"],
+      first_name: payload["given_name"],
+      last_name: payload["family_name"],
+      name: payload["email"],
+      avatar: payload["picture"],
+    };
+    res.json({
+      user: user,
+      token: jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, {
+        expiresIn: "1h",
+      }),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/facebook", function (req, res, next) {
+  passport.authenticate("facebook-token", function (err, user, info) {
+    if (err) res.status(401);
+    if (user) {
+      res.status(200).json({
         user: user,
-        token: jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, {
+        access_token: jwt.sign(user, process.env.ACCESS_TOKEN_SECRET_KEY, {
           expiresIn: "1h",
         }),
       });
-    } catch (error) {
-      console.log(error)
+    } else {
+      res.status(401);
     }
-  }
-)
+  })(req, res, next);
+});
 
 module.exports = router;
--- modules/passport/index.js
+++ modules/passport/index.js
@@ -2,14 +2,15 @@ const passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy;
 const pool = require("../../config-db");
 
-const JwtStrategy = require('passport-jwt').Strategy,
-  ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const FacebookTokenStrategy = require("passport-facebook-token");
 
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
-  "normal_login",new LocalStrategy(
+  "normal_login",
+  new LocalStrategy(
     { usernameField: "email", passwordField: "password", session: false },
     async function (email, password, done) {
       await pool
@@ -29,6 +30,29 @@ passport.use(
         .catch((err) => {
           return done(err);
         });
+    }
+  )
+);
+
+passport.use(
+  "facebook-token",
+  new FacebookTokenStrategy(
+    {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      fbGraphVersion: "v3.0",
+      passReqToCallback: true,
+    },
+    function (req, accessToken, refreshToken, profile, done) {
+      const user = {
+        first_name: profile.name.givenName,
+        last_name: profile.name.familyName,
+        email: profile.emails[0].value,
+      };
+      // User.findOrCreate({ facebookId: profile.id }, function (error, user) {
+      //   return done(error, user);
+      // });
+      return done(null, user);
     }
   )
 );
@@ -58,28 +82,27 @@ passport.use(
 //   )
 // );
 
-const opts = {}
+const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.ACCESS_TOKEN_SECRET_KEY;
-passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
-  await pool
-    .query(
-      'SELECT id FROM "user" WHERE id=$1',
-      [jwt_payload.id]
-    )
-    .then((result) => {
-      if (result.rows.length !== 0) {
-        return done(null, result.rows[0]);
-      } else {
-        return done(null, false, {
-          message: "Invalid token!",
-        });
-      }
-    })
-    .catch((err) => {
-      return done(err);
-    });
-}));
+passport.use(
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    await pool
+      .query('SELECT id FROM "user" WHERE id=$1', [jwt_payload.id])
+      .then((result) => {
+        if (result.rows.length !== 0) {
+          return done(null, result.rows[0]);
+        } else {
+          return done(null, false, {
+            message: "Invalid token!",
+          });
+        }
+      })
+      .catch((err) => {
+        return done(err);
+      });
+  })
+);
 
 // passport.use(new MailruStrategy({
 //   clientID: "780592097647-hif1svldddrkc4jpojqc44paile3l8da.apps.googleusercontent.com",
@@ -91,16 +114,20 @@ passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
 //   }
 // ));
 
-passport.use(new GoogleStrategy({
-  clientID: "780592097647-hif1svldddrkc4jpojqc44paile3l8da.apps.googleusercontent.com",
-  clientSecret: "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",
-  callbackURL: "http://localhost:3000"
-},
-  function (token, tokenSecret, profile, done) {
-    console.log(token);
-    console.log(tokenSecret);
-    console.log();
-    return done(null, {});
-  }
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "780592097647-hif1svldddrkc4jpojqc44paile3l8da.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:3000",
+    },
+    function (token, tokenSecret, profile, done) {
+      console.log(token);
+      console.log(tokenSecret);
+      console.log();
+      return done(null, {});
+    }
+  )
+);
 module.exports = passport;
